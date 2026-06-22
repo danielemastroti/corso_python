ef777fec28b62bad09cb3085fdf34cd3f8209168
MASTROTI DANIELE <daniele.mastroti@intesasanpaolo.com>
2026-06-22 08:20:49 +0000

    last

--- 5-strutture_dati.py
+++ 5-strutture_dati.py
@@ -5,10 +5,42 @@
 
 
 # LISTE
-Frutta=("Banana", "Ciliegia", "Albicocca", "Anguria")
-print(Frutta)
-# slicing
-print(Frutta[1])
+# Frutta=["Banana", "Ciliegia", "Albicocca", "Anguria"]
+# print(Frutta)
+# # slicing
+# print(Frutta[1])
 
-for item in Frutta:
-    print(item)
+# for item in Frutta:
+#     print(item)
+
+# Frutta.append("Kiwi")
+# print(Frutta)
+# Frutta.sort()
+# print(f"Frutta sorted: {Frutta}")
+
+# Frutta.remove("Banana")
+# print(Frutta)
+
+
+
+# # TUPLE (immutabili)
+# Auto=("Ford", "Opel", "Dacia", "Lamborghini", "Ford", "Ford")
+# print(Auto)
+# print(Auto.count("Ford"))
+
+# # da errore
+# #Auto.append("Ferrari")
+
+# # Dizionari
+# router={
+#     "IP": "1.1.1.1",
+#     "Hostname": "rtr1",
+#     "Sede": "Via Bisceglie,120"
+# }
+
+# print(router)
+# print(router["IP"])
+
+# for key, value in router.items():
+#     print(f"{key}: {value}")
+
